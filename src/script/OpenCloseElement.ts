9d7daafcfe7f05d48c238e6e7ad505e0611a175b
azertypow <azertypow@gmail.com>
2019-03-23 13:53:30 +0100

    feat: add open/close card

---
+++ src/script/OpenCloseElement.ts
@@ -0,0 +1,29 @@
+export default class OpenCloseElement {
+
+  set _open(value: boolean) {
+    // @ts-ignore: force write value
+    this.__open = value
+    this._elementToOpenClose.dataset.open = value.toString()
+  }
+  private readonly __open = false
+
+  constructor(
+    private _elementToOpenClose: HTMLElement,
+    private _elementForOpen: HTMLElement,
+    private _elementForClosed = _elementForOpen) {
+
+    console.log(_elementForOpen)
+
+    if(_elementForClosed === _elementForOpen) {
+      _elementForOpen.addEventListener("click", this._setEventToggle_open.bind(this))
+    } else {
+
+    }
+  }
+
+  private _setEventToggle_open() {
+    this._open = !this.__open
+  }
+  private _setEventOpen_open() {}
+  private _setEventClose_open() {}
+}
