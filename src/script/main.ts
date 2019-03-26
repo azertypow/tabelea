d7c567b9dc74994d1c4b376b9529b14078b54975
azertypow <azertypow@gmail.com>
2019-03-26 13:43:14 +0100

    chore: refactoring

--- src/script/main.ts
+++ src/script/main.ts
@@ -1,10 +1,10 @@
-import initScrollOnHeaderFixedText from "./initScrollOnHeaderFixedText"
-import initClickNavigationOnProjectImages from "./initClickNavigationOnProjectImages"
-import initAutomaticTimerImageNavigation from "./initAutomaticTimerImageNavigation"
-import initDescriptionToHeaderSystem from "./initDescriptionToHeaderSystem"
-import initCardDraggabilly from "./initCardDraggabilly"
-import initLoopScrolling from "./initLoopScrolling"
-import initOpenCloseContact from "./initOpenCloseContact"
+import initScrollOnHeaderFixedText from "./_initScrollOnHeaderFixedText"
+import initClickNavigationOnProjectImages from "./_initClickNavigationOnProjectImages"
+import initAutomaticTimerImageNavigation from "./_initAutomaticTimerImageNavigation"
+import initDescriptionToHeaderSystem from "./_initDescriptionToHeaderSystem"
+import initCardDraggabilly from "./_initCardDraggabilly"
+import initLoopScrolling from "./_initLoopScrolling"
+import initOpenCloseContact from "./_initOpenCloseContact"
 import PercentMarginTopPosition from "./PercentMarginTopPosition"
 
 initScrollOnHeaderFixedText()
